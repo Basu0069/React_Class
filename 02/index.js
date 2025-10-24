44c4f26e70bce40f7b6cd834511b9513bda71d9c
Basu0069 <netflixbasu1@gmail.com>
2025-10-24 14:05:34 +0530

    updated

--- 02/index.js
+++ 02/index.js
@@ -88,11 +88,92 @@
 
 
 // index of 
-let nums = [2, 6, 8, 10, 11, 14, 21, 27];
+// let nums = [2, 6, 8, 10, 11, 14, 21, 27];
 
-let result = nums.indexOf(10);
+// let result = nums.indexOf(10);
 
-console.log(result);
+// console.log(result);
+
+
+
+
+
+// map fiunction 
+// let fruits = ["mango", "apple", "banana"];
+
+// fruits.map((x, y) => console.log(x, y));
+
+
+
+
+
+
+// let nums = [8, 12, 18, 28, 34, 62, 128, 218, 512, 810];
+
+// nums.map((num, index) => {
+//   console.log(`Index: ${index}, Value: ${num}`);
+// });
+
+
+
+// let sum = 0;
+// nums.forEach(num => sum += num);
+// console.log(sum);
+
+
+
+
+// another method using reduce 
+// it will take 2 argument first function adn second argument 
+// nums.reduce(() =>{ }, 0)
+
+
+
+
+
+// let sum = nums.reduce((total, num) => total + num, 0);
+// console.log(sum);
+
+
+
+
+
+// Reverse Function 
+// let nums = [8, 12, 18, 28, 34, 62, 128, 218, 512, 810];
+
+// nums.reverse();
+// console.log(nums);
+
+
+
+
+
+
+
+
+
+
+
+// // Spread funtion 
+// let nums = [8, 12, 18, 28, 34, 62, 128, 218, 512, 810];
+// let fruits = ["mango", "apple", "banana"];
+
+// console.log(fruits,nums);
+// console.log(...fruits,...nums);
+
+
+
+
+// Destruct 
+let fruits = ["mango", "apple", "banana"];
+let [a,b,c] = fruits 
+console.log(a,b,c);
+
+
+
+
+
+
 
 
 
